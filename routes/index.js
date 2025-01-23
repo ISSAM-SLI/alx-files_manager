9c938604b7c6abff5136efe9d2b55fc5e1b391c7
ISSAM-SLI <issam.slimani.maroc@gmail.com>
2025-01-23 03:30:02 -0800

    Task 3

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,22 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
-const app = express();
+const router = express.Router();
 
-app.get('/', (req, res) => { res.send('Hello World!\n'); });
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
 
-app.get('/status', (req, res) => { res.send(AppController.getStatus()); });
-
-app.get('/stats', (req, res) => { res.send(AppController.getStats()); });
-
-module.exports = app;
+export default router;
